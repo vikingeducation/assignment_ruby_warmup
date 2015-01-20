2cce8c72b83c6130c72c962eb70ece54cf9789ef
Schwad <nicholas.schwaderer@gmail.com>
2015-01-20 00:37:55 +0000

    dice outcomes method insert

--- roll_dice.rb
+++ roll_dice.rb
@@ -5,3 +5,16 @@ def roll_dice (num=1)
   end
   return total_roll
 end
+
+def dice_outcomes(dice=1,rollno=1) 
+  visual_chart_hash = Hash.new("")
+  rollno.times do 
+    result = roll_dice(dice)
+    visual_chart_hash[result.to_s.to_sym] += "#"
+  end
+  final_return = Hash[visual_chart_hash].sort
+  puts final_return
+  return final_return
+end
+
+dice_outcomes(3,100)

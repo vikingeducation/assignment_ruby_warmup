8e32778c6320a868d646bc67a0ff85ed245f3199
alpha <boris.shporkin@gmail.com>
2015-01-21 21:52:37 -0500

    warmups complete

--- anagrams.rb
+++ anagrams.rb
@@ -1,20 +1,16 @@
-dictionary = File.open("enable.txt", "r").readlines.each.to_a
 def anagrams(word)
-	# puts dictionary.readlines.each
-	# puts "Smith"[0]
-	# puts "Smith"[1]
-	# puts "Smith"[2]
-	# puts "Smith"[3]
-	# puts "Smith"[4]
-	unique_letters=word.split(//).uniq
-	# puts unique_letters
-	word_hash=[]
-	i=0
-	while i<unique_letters.size
-		word_hash[unique_letters[i]]=word.count(unique_letters[i])}
-		i=i+1
+	sorted_word=word.split(//).sort
+	dictionary = File.open("enable.txt", "r").readlines.each.to_a
+	anagrams_array=[]
+
+	dictionary.each do |d|
+		sorted_dictionary_word=d.strip.split(//).sort
+		if (word!=d && sorted_dictionary_word==sorted_word)
+			anagrams_array<<d
+		end
 	end
-	puts word_hash
+
+	puts anagrams_array
 end
 
-anagrams("peeaarrs")
+anagrams("pears")

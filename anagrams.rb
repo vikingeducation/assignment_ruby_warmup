3694ff30aa08323c4321834973f43ad4195daafd
Daniel Au <dakmau@gmail.com>
2015-01-22 14:22:25 -0500

    Finish anagrams.rb with bug

---
+++ anagrams.rb
@@ -0,0 +1,30 @@
+def anagrams word
+  textfile =  File.new("enable.txt", "r")
+  dictionary_of_words = textfile.readlines
+  anagrams_of_word = []
+  dictionary_of_words.each do |item|
+    if (is_an_anagram_of?(word, item))
+      anagrams_of_word.push(item)
+    end
+  end
+  return anagrams_of_word
+end
+
+def is_an_anagram_of? (my_word, a_word)
+  if(my_word.length == a_word.length )
+    my_word_split = my_word.split("")
+    a_word_split = a_word.split("")
+
+    a_word_split.each do |item|
+      if (my_word_split.include?(item))
+      else
+        return false
+      end
+    end
+    return true
+  else
+    return false
+  end
+end
+
+anagrams "hello"

d3b0bd130fdd0597af5e572d2e9885f2b7bcb9c1
Dan Berger <dsberger@gmail.com>
2015-01-27 14:21:55 -0800

    I learned #each_with_object and applied it to my anagrams method

--- anagrams.rb
+++ anagrams.rb
@@ -4,15 +4,14 @@ def anagrams(word)
   f.each_line do |line|
     dictionary << line.strip
   end
-  legal_anagrams = []
-  dictionary.each do |entry|
+  dictionary.each_with_object([]) do |entry, legal_anagrams|
     if are_these_anagrams?(entry.upcase,word.upcase)
       legal_anagrams << entry.upcase
     end
   end
-  legal_anagrams
 end
 
 def are_these_anagrams?(word1,word2)
-  word1 != word2 && word1.split("").sort == word2.split("").sort
+  word1 != word2 &&
+  word1.split("").sort == word2.split("").sort
 end

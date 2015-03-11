086c76f2d7817d1034effad0c6c46c0e0d148d60
niconan <nick.hax@gmail.com>
2015-03-11 14:21:51 -0700

    last tab fix

--- roll_dice.rb
+++ roll_dice.rb
@@ -1,9 +1,9 @@
 def roll_dice( dice = 1 )
-	total = 0
-	dice.times do
-		total += rand(7)
-	end
-	puts "#{total}"
+  total = 0
+  dice.times do
+    total += rand(7)
+  end
+  puts "#{total}"
 end
 
 roll_dice( 4 )

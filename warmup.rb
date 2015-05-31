b4981e411fafbc9f5cd047b79128cf9039a560d0
Justin Mullis <justin.mullis@gmail.com>
2015-05-30 21:45:00 -0400

    dice_outcomes method

--- warmup.rb
+++ warmup.rb
@@ -1,7 +1,45 @@
 def roll_dice(num=1)
   total = 0
+  # For however many dice we have do a roll and add onto the total
   num.times do |i|
     total += (1..6).to_a.sample
   end
   return total
+end
+
+
+def dice_outcomes(num_of_dice, num_of_rolls)
+  
+  how_many = {}
+  
+  num_of_rolls.times do |roll| 
+
+    # Here's our roll_dice method again, just need to get a total
+    # Helpful to imagine we're rolling one at a time and then adding them
+    total = 0    
+    num_of_dice.times do |i|
+      total += (1..6).to_a.sample
+    end
+
+    # Now we're seeing if we already have that total in the hash
+    # that will be used to output our chart.  If we do we go ahead and 
+    # increment it one, otherwise we create it.
+    how_many.has_key?(total) ? how_many[total] += 1 : how_many[total] = 1
+
+  end
+
+  # Sorting the how_many hash so so out output below is in order
+  how_many = how_many.sort.to_h
+
+  # Interate through the hash, puts each key and then a number of pound signs
+  # determined by the value.  Probably a more elegant way to get the spacing
+  # correct.
+  how_many.each do |key, value|
+    if key < 10
+      puts "#{key}:  " + ("#" * value)
+    else
+      puts "#{key}: " + ("#" * value)
+    end
+  end
+
 end

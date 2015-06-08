8bd270f35c6b6213e8707792f323b8db1e3464a0
Garrett <garrettweinert@gmail.com>
2015-06-07 21:52:25 -0700

    finished anagram problem.

--- ruby_problems.rb
+++ ruby_problems.rb
@@ -38,7 +38,51 @@ def dice_outcomes(number_of_dice, number_of_roles)
   possible_numbers.sort.each do |key, value|
     puts "#{key}: #{value}"
   end
-  # puts "#{possible_numbers.keys}: #{possible_numbers.values}"
 end
 
-dice_outcomes(2, 100)
+# dice_outcomes(2, 100)
+
+def fibonacci(number_of_digits)
+  fib_array =[1, 1]
+
+  (3..number_of_digits).each do |i|
+    next_fib = fib_array[i-2]+fib_array[i-3]
+    fib_array.push(next_fib)
+  end
+
+  if(number_of_digits == 1)
+    puts "[#{fib_array[0]}]"
+  else
+  puts "#{fib_array}"
+  end
+end
+
+# fibonacci(7)
+
+# def stock_picker(stock_array)
+#   end
+# end
+
+def anagrams(string)
+
+  return_array=[]
+  string_array=[]
+
+
+  string.each_char {|c| string_array.push(c)}
+
+  file = File.open("enable.txt", 'r')
+  while !file.eof?
+    line = file.readline
+    line = line.strip
+    if string.chars.sort == line.chars.sort
+      return_array.push(line)
+    end
+
+  end
+  puts return_array
+end
+
+
+
+# anagrams("pears")

e54b3f28e7f2e417ad2fb83acdc7f06ff6e6cbad
Garrett <garrettweinert@gmail.com>
2015-06-18 12:09:06 -0700

    final commit all problems completed

--- ruby_problems.rb
+++ ruby_problems.rb
@@ -82,7 +82,7 @@ def stock_picker(stock_array)
   puts "[#{return_buy_day}, #{return_sell_day}]"
 end
 
-stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+# stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
 
 def anagrams(string)
 

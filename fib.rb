db12a16d91b2c2d227dca8d62f860d8b5141337d
Chris Scavello <bideowego@gmail.com>
2015-08-03 16:33:28 -0400

    Change recursive fib function to lambda

--- fib.rb
+++ fib.rb
@@ -1,12 +1,17 @@
+# def rfib(n)
+# 	return n if n == 0 || n == 1
+# 	return rfib(n - 1) + rfib(n - 2)
+# end
+
 def fib(n)
-	def rfib(n)
-		return n if n == 0 || n == 1
-		return rfib(n - 1) + rfib(n - 2)
-	end
 	fibs = []
+	rfib = lambda do |n|
+		return n if n == 0 || n == 1
+		return rfib.call(n - 1) + rfib.call(n - 2)
+	end
 	i = 0
 	while i <= n
-		fibs << rfib(i)
+		fibs << rfib.call(i)
 		i += 1
 	end
 	fibs

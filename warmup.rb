78ec5522f80bce36283c4a80be99f52355fb07c4
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-26 21:54:42 -0400

    add fibonacci method

--- warmup.rb
+++ warmup.rb
@@ -10,23 +10,42 @@ def roll_dice(number_dice=1)
   return result
 end
 
-def dice_outcomes(number_dice=1,number_rolls=100)
+def dice_outcomes(number_dice=2,number_rolls=100)
   results = []
-  index = 0
-  number_rolls.times do
+  number_rolls.times do |current_num|
     results.push(roll_dice(number_dice))
-    index++
   end
 
   results.sort!
 
   last_result=0
-  each results do |result|
+  results.each do |result|
+    spacer = ""
+    if result < 10
+      spacer = " "
+    end
     if result != last_result
-      puts "${result.to_s}: \#"
+      print "\n#{result}:#{spacer} \#"
     else
       print "\#"
     end
+
+    last_result=result
+  end
+
+  print "\n"
+
+  return nil
+end
+
+def fibonacci(number)
+  results = []
+  current = 1
+  last = 0
+  number.times do |iteration|
+    results.push(current)
+    current += last
+    last = current
   end
 
   return results

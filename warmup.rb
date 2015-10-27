a1c5d9f7a9712b169030cb1fc794e7717b54e614
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-26 21:09:59 -0400

    add dice_outcomes method

--- warmup.rb
+++ warmup.rb
@@ -9,3 +9,25 @@ def roll_dice(number_dice=1)
 
   return result
 end
+
+def dice_outcomes(number_dice=1,number_rolls=100)
+  results = []
+  index = 0
+  number_rolls.times do
+    results.push(roll_dice(number_dice))
+    index++
+  end
+
+  results.sort!
+
+  last_result=0
+  each results do |result|
+    if result != last_result
+      puts "${result.to_s}: \#"
+    else
+      print "\#"
+    end
+  end
+
+  return results
+end

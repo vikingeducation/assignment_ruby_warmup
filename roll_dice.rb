84e185af46df7f6add5cc1393a3abd4986c5e1dd
sunsplat <heeycutie@yahoo.com>
2015-12-28 18:19:06 -0500

    finish dice_outcomes

--- roll_dice.rb
+++ roll_dice.rb
@@ -10,14 +10,24 @@ roll_dice(6)
 roll_dice(4)
 
 def dice_outcomes(num_dice, roll_times)
-	num_dice.times do
+		dice_array = []
+		dice_sum = []
 		roll_times.times do
-			rand(1..6)
+			num_dice.times do
+				dice_array.push(rand(1..6))
+			end
+			dice_key = dice_array.inject { |sum, x | sum + x }
+			dice_sum.push(dice_key)
+			dice_array = []
 		end
-	end
+		count_sum = Hash.new 0
+		dice_sum.each do |number|
+			count_sum[number] += 1
+		end
+		count_sum.each { |key, value| puts "#{key}: #{value.times do print '#' end }" }
 end
 
-dice_outcomes(3, 100)
+dice_outcomes(3, 20)
 
 def fibonacci(num)
 	(1..num).map {|i| fibonacci(i)}

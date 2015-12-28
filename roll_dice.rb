ab5ce74225fb78ba882187393852815dca536e70
sunsplat <heeycutie@yahoo.com>
2015-12-28 12:27:41 -0500

    starting dice_outcomes

--- roll_dice.rb
+++ roll_dice.rb
@@ -1,24 +1,32 @@
 def roll_dice(num)
-	return num * 6
+	dice_array = []	
+	num.times do
+		dice_array.push(rand(1..6))
+	end
+	dice_array.inject { |sum, x | sum + x }
 end
 
 roll_dice(6)
 roll_dice(4)
 
-def dice_outcomes(num, roll_times)
-
+def dice_outcomes(num_dice, roll_times)
+	num_dice.times do
+		roll_times.times do
+			rand(1..6)
+		end
+	end
 end
 
+dice_outcomes(3, 100)
+
 def fibonacci(num)
-	fib_array = []
+	(1..num).map {|i| fibonacci(i)}
 	if num == 1
 		1
 	elsif num == 2
 		1
 	else 
-		num.times do 
-			fibonacci(num-1) + fibonacci(num-2)
-		end
+		fibonacci(num-1) + fibonacci(num-2)
 	end
 end
 

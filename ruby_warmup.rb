95160491e6e6434e186a3323ad054ed62c9106ad
Julia Herron <julia.herron@gmail.com>
2015-12-29 11:06:13 -0800

    stock picker function, corrected comment format

--- ruby_warmup.rb
+++ ruby_warmup.rb
@@ -5,16 +5,6 @@ def single_roll
 end
 
 def roll_dice(number_of_dice=1)
-  dice_rolled = 0
-  total_score = 0
-  while dice_rolled < number_of_dice
-    total_score += single_roll
-    dice_rolled += 1  
-  end
-  return total_score
-end
-
-def roll_dice_using_times(number_of_dice=1)
   total_score = 0
   number_of_dice.times do |number_of_dice|
     total_score += single_roll
@@ -23,37 +13,22 @@ def roll_dice_using_times(number_of_dice=1)
 end
 
 
-
 # Rolling Dice Outcomes
-"""
-number_of_dice: how many dice you're rolling each time
-number_of_rolls: how many times you roll all dice
-return: list of all possible totals with the number of occurrences shown as a # sign
 
-example: 
-rolling_dice(3, 100)
-  3: #
-  4: ###
-  5: #####
-  6: ....
-  18: ##
+# number_of_dice: how many dice you're rolling each time
+# number_of_rolls: how many times you roll all dice
+# return: list of all possible totals with the number of occurrences shown as a # sign
 
-step 1, make_outcomes: create a hash of all possible totals, min = number_of_dice * 1, max = number_of_dice * 6
+# example: 
+# rolling_dice(3, 100)
+#   3: #
+#   4: ###
+#   5: #####
+#   6: ....
+#   18: ##
 
-step 2: run single_roll function number_of_rolls times; each time it runs, add a # to the value whose key matches the total
-"""
+# step 1, make_outcomes: create a hash of all possible totals, min = number_of_dice, max = number_of_dice * 6
 def make_outcomes(number_of_dice)
-  outcomes = {}
-  start = number_of_dice 
-  max_total = number_of_dice * 6
-  while start <= max_total
-    outcomes[start] = ""
-    start += 1
-  end
-  return outcomes
-end
-
-def make_outcomes_with_each(number_of_dice)
   outcomes = {}
   dice_array = (number_of_dice..(number_of_dice*6)).to_a
   dice_array.each do |total|
@@ -62,7 +37,7 @@ def make_outcomes_with_each(number_of_dice)
   return outcomes
 end
 
-
+# step 2: run single_roll function number_of_rolls times; each time it runs, add a # to the value whose key matches the total
 def rolling_dice_outcomes(number_of_dice, number_of_rolls)
   outcomes = make_outcomes(number_of_dice)
   i = 0
@@ -78,11 +53,10 @@ end
 
 # Fibonacci
 
-def fibonacci(n, sequence=[1])
-  """
-  Fn = Fn-1 + Fn-2 
-  ^^^ with seed values F1 = 1, F2 = 1
-  """
+def fibonacci(n)
+  # Fn = Fn-1 + Fn-2 
+  # ^^^ with seed values F1 = 1, F2 = 1
+  sequence = [1]
   (n-1).times do
     current_number, last_number = sequence.last(2)
     sequence << current_number + (last_number or 0)
@@ -90,3 +64,33 @@ def fibonacci(n, sequence=[1])
   return sequence
 end
 
+
+
+# Stock picker
+  # input: an array of stock prices
+  # output: the most profitable days to buy and sell the stock
+  # example:
+  # > stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15]) 
+  #=> [2, 6]
+
+def stock_picker(days)
+  buy_day = 0
+  maximum = 0
+  best_days = []
+
+  while buy_day <= days.length
+    buy_price = days[buy_day]
+
+    (buy_day..days.length-2).each do |possible_sell_day|
+      sell_price = days[possible_sell_day+1]
+      profit = sell_price - buy_price
+
+      if profit > maximum
+        maximum = profit
+        best_days = buy_day, possible_sell_day+1
+      end
+    end
+    buy_day += 1
+  end
+  return best_days
+end

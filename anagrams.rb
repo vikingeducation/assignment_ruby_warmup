3ac78bfc7cec5b131832c8f42670bee3c8c24ddf
Joe Bernardi <joebernardi@hotmail.com>
2016-05-13 12:58:31 -0500

    working anagram, need var in regex to work

--- anagrams.rb
+++ anagrams.rb
@@ -3,21 +3,21 @@
 =end
 
 # PROGRAM ANAGRAMS
-def anagrams(word)
+def anagrams
 
+	word = "pears"
 # READ THE FILE AND STORE INTO AN ARRAY
 	dictionary_array = IO.readlines('/Users/JoeBernardi/VCS/Dictionary/dictionary.txt')
-
+regex = /^(?!.*(.).*\1)[pears]*$/
 # DELETE /N FROM THE END OF EACH LINE
 	dictionary_array.each { |w| w.delete!("\n")}
-
 # SET UP THE NEW ARRAY TO STORE THE ANAGRAMS
 	anagram_array = []
 
 # CHECK EACH ITEM IN THE ARRAY TO SEE IF IT CONTAINS THE LETTERS
 	dictionary_array.each { |k|
 		# EVERY LETTER MUST BE PRESENT AND NO REPEATS UNLESS IN THE WORD
-		if k.match(/^[pears]$/) && (k.length == word.length)
+		if k.match(regex) && (k.length == word.length && k != word)
 			anagram_array << k
 		end }
 
@@ -26,4 +26,4 @@ print anagram_array
 end
 
 
-anagrams("pears")
+anagrams

3238724b13fd8c222ef6d75e91b0b77c5d89c062
Joe Bernardi <joebernardi@hotmail.com>
2016-05-12 20:53:30 -0500

    refactor outcomes chaining count, inject and puts

--- dice_outcomes.rb
+++ dice_outcomes.rb
@@ -1,10 +1,6 @@
 #PROGRAM DICE OUTCOMES
 def dice_outcomes(number_of_dice=1, number_of_rolls=1)
 
-number_of_dice.to_i
-number_of_rolls.to_i
-
-
 outcomes = []
 
 # ROLL THE NUMBER OF DICE x TIMES
@@ -14,24 +10,18 @@ number_of_rolls.times {
 	outcomes << roll_dice(number_of_dice)
 }
 
-# COUNT THE NUMBER OF OCCURENCES OF EACH NUMBER
-dice_hash = outcomes.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
-
-# SORT THE HASH
-dice_hash.sort.each { |k, v|	puts "#{k}: #{"#"*v}"}
+# COUNT THE NUMBER OF OCCURENCES OF EACH NUMBER INJECTED IN NEW HASH
+# SORT THE HASH AND PRINT THE RESULT
+outcomes.inject(Hash.new(0)) { |total, e| total[e] += 1; total }.sort.each { |k, v|	puts "#{k}: #{"#"*v}"}
 
 end
 
 def roll_dice(number_of_dice=1)
-
-# NEED AN ARRAY TO STORE EACH ROLL OF THE DIE
-number_of_dice.to_i unless number_of_dice.is_a?(Fixnum)
-
 # USING NUMBERS 1-6 FOR SIDES
 roll_number = (1..6).to_a
 result = 0
 
-# TAKING X NUMBER OF DICE
+# TAKING x NUMBER OF DICE
 number_of_dice.times {
 # FOR EACH DIE GENERATE A NUMBER ADD TO RESULT
 	result += roll_number.sample
@@ -40,4 +30,5 @@ number_of_dice.times {
 return result
 
 end
-dice_outcomes(2,100)
+
+dice_outcomes(5,50)

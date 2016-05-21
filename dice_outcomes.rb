14fb7d1f5e5949207228cfefecde6680ece2df6a
Graham Turner <turnertgraham@gmail.com>
2016-05-21 15:55:08 -0400

    completed dice_outcomes method

---
+++ dice_outcomes.rb
@@ -0,0 +1,21 @@
+def dice_outcomes(num_dice = 1, num_rolls = 1)
+	numbers = {}
+	num_rolls.times do |roll|
+		number_rolled = 0
+		num_dice.times do |dice|
+			number_rolled += rand(6) + 1
+		end
+		number_rolled = number_rolled
+		if numbers[number_rolled] == nil
+			numbers[number_rolled] = 1
+		else
+			numbers[number_rolled] += 1
+		end
+	end
+	numbers = numbers.sort
+	numbers.each do |key, value|
+		puts "#{key}: #{"#" * value}"
+	end
+end
+
+dice_outcomes(3, 100)

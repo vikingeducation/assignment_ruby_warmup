3ddbe6b1843960de6e1e5cb78cdf22d7e38f26d3
jsteenb2 <jonathansteenbergen@gmail.com>
2016-05-29 18:22:02 -0700

    fixed bug with anagram list including input word

--- anagrams.rb
+++ anagrams.rb
@@ -3,8 +3,9 @@ def anagrams(word)
 	dictionary.map! {|line| line.strip}
 
 	permed_words = word.split(//).permutation.to_a.sort.map {|ele| ele.join}
-	permed_words.select {|perm_word| dictionary.include?(perm_word) && (perm_word != word) }
-	# anagrams = dictionary.select {|dict_word| permed_words.include?(dict_word) && (dict_word != word) }
+	permed_words.delete(word)
+	permed_words.select {|perm_word| dictionary.include?(perm_word)}
+	# anagrams = dictionary.select {|dict_word| permed_words.include?(dict_word)}
 end
 
 puts anagrams("pears")

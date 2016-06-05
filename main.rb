fd1a59fb24c7df43bed6ef8ae8ed78031debdaae
Conor Fischer <conor.fischer@gmail.com>
2016-06-05 16:33:10 -0500

    Add dice_outcomes method

--- main.rb
+++ main.rb
@@ -5,3 +5,25 @@ end
 puts roll_dice(2)
 puts roll_dice(4)
 puts roll_dice()
+puts "-"*9
+
+def dice_outcomes(number_of_dice, dice_rolls)
+	dice_stats = {}
+	dice_rolls.times do
+		current_roll = roll_dice(number_of_dice)
+		#Add dice roll to stats hash
+		if dice_stats[current_roll].nil?
+			dice_stats[current_roll] = 1
+		else
+			dice_stats[current_roll] += 1
+		end
+	end
+
+	#Visual output of stats of hash
+	dice_stats.sort.each do | key, value|
+		puts key.to_s + ': ' + value.to_s
+	end
+end
+
+dice_outcomes(3,100)
+puts "-"*9

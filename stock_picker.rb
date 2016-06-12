3bfb066a614f9bac5f27a8998b225caeac0be1fa
Vishal Lama <vishal.lama@gmail.com>
2016-06-12 10:33:04 -0600

    Update implementation of the stock_picker method
    Remove the use of an offset variable to keep track of the indices
    of future stock prices. Use the Enumerator#with_index(offset) method.

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,17 +3,15 @@ def stock_picker(stock_prices)
   index_low = 0
   index_high = 0
   max_index = stock_prices.length - 1
-  offset = 0
 
   stock_prices.each_with_index do |stock_price, index|
-    offset += 1
     future_stock_prices = stock_prices[(index + 1)..max_index]
-    future_stock_prices.each_with_index do |future_stock_price, i|
+    future_stock_prices.each.with_index(index + 1) do |future_stock_price, i|
       new_profit = future_stock_price - stock_price
       if new_profit > max_profit
         max_profit = new_profit
         index_low = index
-        index_high = i + offset
+        index_high = i
       end
     end
   end

2c64696600758e3ce89a0604d7059fe4b1ecf94b
Adrian Mui <adrian.mui@gmail.com>
2016-06-16 03:58:22 -0500

    fixed stockpicker

--- methods.rb
+++ methods.rb
@@ -59,7 +59,7 @@ class Test
         if val2 - val1 > profit
           profit = val2 - val1
           findex[0] = index1
-          findex[1] = index2
+          findex[1] = index2 + index1
         end
       end
     end
@@ -83,13 +83,14 @@ class Test
 end
 
 # testing outputs of functions
-# theTester = Test.new
+theTester = Test.new
 # print " * * * * * testing stuff * * * * * \n"
 
 # test1 = theTester.roll_dice(3)
 # test2 = theTester.dice_outcomes(3,100)
 # test3 = theTester.fibonacci(7)
-# test4 = theTester.stock_picker([44,30,24,32,35,30,40,38,15])
+test4 = theTester.stock_picker([44,30,24,32,35,30,40,38,15])
+print test4
 # test5 = theTester.anagrams("pears")
 
 # print test1 ,"\n", test2, "\n", test3, "\n", test4, "\n", test5, "\n"

3e1e546db09803e3c38cce8e67002dfbe1577547
“Matthew <matthew.hinea@gmail.com>
2016-06-23 18:46:24 -0400

    finishs stock picker method

--- ruby_warmup.rb
+++ ruby_warmup.rb
@@ -41,3 +41,32 @@ def fibonacci n
 	return sequence
 end
 
+def stock_picker stock_values 
+	#iterate through the array, keeping track of indices
+	#produce an array of remaining days each iteration
+	#calculate the difference between the current value and the highest value in the remaining days array (highest- current)
+	#if that difference is greater than the previous calculated greatest difference, update the greatest difference value
+	#and store the index of the current day and the day of the max price from the remaining array
+	greatest_difference = 0
+	sell_day = 0
+	buy_day = 0
+	most_profitable_pair = []
+
+	index = 0 
+	while index < stock_values.length - 1
+		current_value = stock_values[index]
+		remaining_prices = stock_values[(index+1)..(stock_values.length-1)]
+		next_highest = remaining_prices.max
+		next_highest_index = remaining_prices.index(next_highest) + index + 1
+		current_difference = next_highest - current_value
+		if current_difference > greatest_difference 
+			greatest_difference = current_difference
+			buy_day = index
+			sell_day = next_highest_index
+		end
+		index += 1
+	end
+	most_profitable_pair[0], most_profitable_pair[1] = buy_day, sell_day
+end
+
+puts stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])

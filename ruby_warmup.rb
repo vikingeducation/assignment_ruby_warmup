84d8265ab9318919377312bb42df5605a1ea2fac
“Matthew <matthew.hinea@gmail.com>
2016-06-23 18:12:44 -0400

    finish fibonacci sequence

--- ruby_warmup.rb
+++ ruby_warmup.rb
@@ -5,10 +5,11 @@ def roll_dice(n=1)
 end
 
 def dice_outcomes(dice,n)
+	#get a hash of empty strings first
 	#create an empty hash 
 	#for 'n_times' times, roll 'dice' number of dice
 	#add a '#' to the value of the key (with the key obviously being the value of the dice roll)
-	#get a hash of empty strings first
+	
 	outcomes = {}
 	possible_values = Array(dice..(6*dice))
 	possible_values.each {|possible_value| outcomes[possible_value] = ""}
@@ -28,4 +29,15 @@ def dice_outcomes(dice,n)
 
 end
 
-puts dice_outcomes(3, 100) 
+def fibonacci n
+	#start with an array of the first two numbers
+	#add the last two numbers of the array together and add it to the end of the array until the array is of n length
+
+	sequence = [1, 1]
+	until sequence.length == n 
+		new_value = sequence[-1] + sequence[-2]
+		sequence.push(new_value)
+	end
+	return sequence
+end
+

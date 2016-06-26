8c522f4b5e46dd7a90bf44d80ccbbce4ecd158c0
Christian Carey <christianrcarey@gmail.com>
2016-06-26 00:11:50 -0400

    Complete methods.

---
+++ anagrams.rb
@@ -0,0 +1,17 @@
+def anagrams(word)
+  file = File.open('enable.txt', 'r')	
+  dictionary = file.read.split("\n")
+  file.close
+
+  anagrams = []
+  sorted_word = word.chars.sort.join
+  
+  dictionary.each do |dictionary_word|
+    sorted_dictionary_word = dictionary_word.chars.sort.join
+    if sorted_word == sorted_dictionary_word
+      anagrams << dictionary_word
+    end
+  end
+
+  anagrams
+end

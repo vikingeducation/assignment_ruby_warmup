62c8b6c29a014735da8191fab3bce6414eaa7567
Johnny Hu <jgeehen.hu@gmail.com>
2016-07-04 15:04:56 -0700

    Refactor with Rubocop

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,8 +6,8 @@ def stock_picker(stock_prices)
 
   stock_prices.each_with_index do |buy_price, buy_day|
     # puts "Buy day #{buy_day}, buy price #{buy_price}"
-    
-    stock_prices[buy_day+1..-1].each_with_index do |sell_price, sell_day|
+
+    stock_prices[buy_day + 1..-1].each_with_index do |sell_price, sell_day|
       # puts "Sell day #{sell_day}, sell price #{sell_price}"
 
       if (sell_price - buy_price) > largest_profit
@@ -17,7 +17,9 @@ def stock_picker(stock_prices)
       end
     end
   end
-  puts "If you buy on day #{buy_date} and sell on day #{sell_date}, you'll earn a profit of #{largest_profit}."
+  puts "If you buy on day #{buy_date} and sell on day #{sell_date}"
+  puts "You'll earn a profit of #{largest_profit}."
 end
 
-stock_picker([rand(100), rand(100), rand(100), rand(100), rand(100), rand(100), rand(100), rand(100), rand(100)])
+stock_picker([rand(100), rand(100), rand(100), rand(100),
+              rand(100), rand(100), rand(100), rand(100), rand(100)])

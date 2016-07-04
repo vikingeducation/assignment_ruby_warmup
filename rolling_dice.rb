39ca6cba48c5556f8ee010737acbf21c91e928f6
Johnny Hu <jgeehen.hu@gmail.com>
2016-07-03 23:11:55 -0700

    Add method roll_dice

---
+++ rolling_dice.rb
@@ -0,0 +1,12 @@
+def roll_dice(rolls=1)
+  result = 0
+
+  rolls.times do |roll|
+    value = rand(1..6)
+    result += value
+    puts "Roll #{roll} returns #{value}"
+  end
+  result
+end
+
+puts roll_dice(4)

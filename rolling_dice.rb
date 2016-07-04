8ab41864228243552320a529bd432b0586d67688
Johnny Hu <jgeehen.hu@gmail.com>
2016-07-03 23:59:21 -0700

    Refactor code using Rubocop

--- rolling_dice.rb
+++ rolling_dice.rb
@@ -1,14 +1,14 @@
 def roll_dice(rolls = 1)
   result = 0
 
-  rolls.times do |roll|
+  rolls.times do
     value = rand(1..6)
     result += value
   end
   result
 end
 
-# puts roll_dice(4)
+puts roll_dice(4)
 
 def dice_outcomes(dice, rolls)
   outcome = {}
@@ -19,17 +19,17 @@ def dice_outcomes(dice, rolls)
     # calculates the sum per roll (uses method above).
     roll_result += roll_dice(dice)
 
-    # adds roll_result to hash as key; assigns or, if key already exists, adds "#" to key's value.
+    # adds roll_result to hash as key; assigns or,
+    # if key already exists, adds "#" to key's value.
     if outcome[roll_result].nil?
-      outcome[roll_result] = "#"
+      outcome[roll_result] = '#'
     else
-      outcome[roll_result] += "#"
+      outcome[roll_result] += '#'
     end
   end
 
   # sorts outcome hash, then prints key, value pairings.
   outcome.sort.each do |result, count|
-
     # adds additional space, for formatting purposes.
     if result < 10
       puts "#{result}:  #{count}"

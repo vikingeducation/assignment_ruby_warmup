306dafe30a169a11ef105aa55c3c31e9581efffa
Michael Brenckle <brenckle@gmail.com>
2016-08-22 15:26:02 -0700

    dice outcomes done

--- dice_outcomes.rb
+++ dice_outcomes.rb
@@ -1,17 +1,33 @@
+# method with two arguments
 
-def roll_dice (dice=1, rolls)
-  score = 0
-  while dice > 0
-    roll = rand(1..6).to_i
-    score += roll
-    dice -= 1
+# 1st arg is how many dice to roll
+# 2nd arg is how many times to roll each dice
+
+# tally each possible sum and how many times it occured
+# possible sum as key and total count for sum as value
+
+def dice_outcomes (dice=1, rolls=1)
+  tally = []
+  roll_count = 0
+  while roll_count < rolls
+    score = 0
+    dice.times do |roll|
+      score += rand(1..6).to_i
+    end
+    tally.push score
+    roll_count += 1
   end
-  puts score
-  puts ''
+  counts = Hash.new 0
+  tally.sort.each do |sum|
+    counts[sum] += 1
+  end
+  counts.each do |sum, total|
+    if sum < 10
+      puts "#{sum}:  " + ("#" * total.to_i)
+    else
+      puts "#{sum}: " + ("#" * total.to_i)
+    end
+  end
 end
 
-roll_dice(4)
-roll_dice(2)
-roll_dice(50)
-roll_dice
-
+dice_outcomes(3,100)

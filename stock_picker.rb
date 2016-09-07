b144eff3b1752c161a2f2a2a2b988066a8cb30f8
Visiona <darbis@gmail.com>
2016-09-07 19:43:18 +0100

    Created solution to stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,22 @@
-value=arr[0]
-day1=0
-day2=0
-.each_with_index {|num,i| if num>vaule then  day1=i && day2=day1}
+=begin 
+Stock Picker - a method stock_picker takes an array of stock prices (prices on days 0, 1, ...) and outputs the most profitable pair of days on which to first buy the stock and then sell the stock.
+=end
+
+def stock_picker(arr_days)
+    profit=0
+    arr_profit_days=[]
+    arr_days.each_with_index do |value_min, day_min|
+        arr_days.each_with_index do |value_max, day_max|
+            if day_min<day_max && value_max>value_min && profit<value_max-value_min
+                profit=value_max-value_min
+                arr_profit_days.clear
+                arr_profit_days <<day_min
+                arr_profit_days <<day_max
+            end
+        end
+    end
+    arr_profit_days
+end
+
+#stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+#=> [2, 6]

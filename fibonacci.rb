5ecb745c9f09c4b90df66f2331841af16d2d317a
Visiona <darbis@gmail.com>
2016-09-07 18:51:21 +0100

    Created solution and file to fibonacci task

---
+++ fibonacci.rb
@@ -0,0 +1,9 @@
+def fibonacci(x)
+    fib_array=[]
+    i=0
+    while (x-1)>=i
+        i>1?fib_array << (fib_array[i-1] + fib_array[i-2]):fib_array << 1
+        i+=1
+    end
+    fib_array
+end
--- roll_dice.rb
+++ roll_dice.rb
@@ -1,8 +1,8 @@
 def roll_dice(x=1)
     y = rand(1..6)
-    x==1 ? y:  y + roll_dice(x-1)
+    x==1 ? y:  (y + roll_dice(x-1))
 end
 
-print roll_dice(4)
+roll_dice(4)
 roll_dice(2)
 roll_dice

0f53ee7ab8f7c197a807978c0e25b146c767843f
Francisco Suero <suerofrancisco@yahoo.com>
2016-09-15 09:13:32 -0400

    first exercise modified to allow different dice inputs

--- 1rolldice.rb
+++ 1rolldice.rb
@@ -1,18 +1,18 @@
-def roll_dice(num = 1)
-  dicearray = (1..6).to_a
+#this is a dice roller app, it takes two inputs. How many sides the dice have, and how many of them you want to roll. 
+def roll_dice(num = 1, sides = 6)
+  dicearray = (1..sides).to_a
   holder = 0
   dice = num
   while 0 < num
     holder += dicearray.sample
-    puts holder
     num -= 1 
   end
 
-  puts "The sum of #{dice} six sided dice being rolled is #{holder}"
+  puts "The sum of #{dice} #{sides} sided dice being rolled is #{holder}"
 
 end
 
-roll_dice(1)
-roll_dice(20)
+roll_dice(1, 12)
+roll_dice(20, 4)
 roll_dice
 roll_dice(4)

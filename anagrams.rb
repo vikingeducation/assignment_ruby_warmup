6c059ba29eedaa433079dc3f314705ea1f392a81
libraryman <jeffreystephensabol@gmail.com>
2016-10-14 13:30:43 -0700

    completed anagram.rb

---
+++ anagrams.rb
@@ -0,0 +1,7 @@
+def anagrams(word)
+  words = []
+  lines = IO.readlines("./enable.txt")
+  lines.each { |line| words << line.chomp if word.split("").sort == line.chomp.split("").sort }
+  words
+end
+puts anagrams("pears")

21c9106104d65ac6891d54d45f8456a30c2d5abe
Jake Tingley <jaket32@gmail.com>
2016-10-24 01:11:37 -0400

    finished stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,17 @@
 def stock_picker(prices)
-  current_high = []
+  current_high = 0
+  best_days = []
   
-  prices.each do
-    puts
+  (prices.length).times do |i|
+    (prices.length-1).downto(i) do |j|
+      profit = prices[j]-prices[i]
+      if profit > current_high
+        current_high = profit
+        best_days = [i, j]
+      end
+    end
   end
-# evaluate the profits
-# if current_profit > 
-# place largest into temp array
-#  return current_high
+  puts best_days
 end
 
 stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])

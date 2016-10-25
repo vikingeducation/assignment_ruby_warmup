a8f28a3970f0fc22fec2e46e259d372e8692bc5b
James Harris <computersherpa@gmail.com>
2016-10-24 19:33:29 -0500

    dice_possibilities, dice_outcomes

--- assignment.rb
+++ assignment.rb
@@ -9,5 +9,23 @@ def roll_dice(number_of_dice)
   end
   return sum
 end
-puts roll_dice(4)
-puts roll_dice(2)
+
+def dice_possibilities(number_of_dice)
+  min = number_of_dice
+  max = number_of_dice * 6
+  return Array(min..max)
+end
+
+def dice_outcomes(number_of_dice, number_of_dice_rolls)
+  possibilites = dice_possibilities(number_of_dice)
+  results = Hash[possibilites.map {|i| [i, 0]}]
+  number_of_dice_rolls.times do
+    result = roll_dice(number_of_dice)
+    results[result] += 1
+  end
+  possibilites.each do |i|
+    puts String(i).rjust(2) + ": " + "#" * results[i] + "\n"
+  end
+  return results
+end
+dice_outcomes(3, 100)

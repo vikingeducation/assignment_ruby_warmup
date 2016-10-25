aaa9d89ab8afbeaf7fd645793e5ab48739190ead
James Harris <computersherpa@gmail.com>
2016-10-24 21:17:50 -0500

    stock_picker: Sell only after buying

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,12 +1,16 @@
+#require 'pry'
+
 prices = [44, 30, 24, 32, 35, 30, 40, 38, 15]
 
 def stock_picker(prices)
   pairs = {}
-  prices.each_with_index do |buy, i| #FIXME: This includes pairs that can only be obtained through time travel, i.e. selling things before they have been purchased.
-    prices.each_with_index do |sell, j|
-      pairs[[i, j]] = sell - buy
+  prices.each_with_index do |buy, i|
+    prices[i..prices.length].each_with_index do |sell, j|
+      #binding.pry
+      pairs[[i, j+i]] = sell - buy
     end
   end
+  #puts pairs
   result = pairs.max_by { |key, value| value }
   puts "Most profitable indexes: #{result[0]}. Profit: #{result[1]}"
 end

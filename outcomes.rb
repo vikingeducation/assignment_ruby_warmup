f195638de8c17777bd43fd44995031092626a8b0
tt <ttwang.303@gmail.com>
2016-11-19 18:53:28 -0500

    finish dice outcomes problem

--- outcomes.rb
+++ outcomes.rb
@@ -1,17 +1,22 @@
 def dice_outcomes(number_of_dice, number_of_times)
 
-  frequencies = Hash.new(0)
+#generating keys for frequencies hash
+  keys = (number_of_dice..(number_of_dice*6)).to_a
+  frequencies = Hash[keys.map { |x| [x, 0] } ]
 
 # generating values for frequencies hash
   number_of_times.times do |outcome|
-    outcome = number_of_dice + rand(number_of_dice*6)
+    outcome = 0
+    number_of_dice.times { outcome += (1 + rand(6))}
     frequencies[outcome] += 1
   end
 
-  puts frequencies.keys
-  puts frequencies.values
+#print results in visual chart
+  frequencies.each do |outcome, frequency|
+    puts "#{outcome}:" + ('#' * frequency)
+  end
 
 end
 
 
-dice_outcomes(5, 100)
+dice_outcomes(1, 50)

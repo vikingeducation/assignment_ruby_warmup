30849a1d6aea784e4c98d37a03787d00695fb4f1
yxlau <yixuan.lau@gmail.com>
2016-12-06 09:30:35 +0800

    Use sort instead and compare arrays directly

--- anagrams.rb
+++ anagrams.rb
@@ -1,17 +1,10 @@
 def anagrams(input)
   anagrams = []
-  input = input.upcase
   File.foreach('enable.txt') do |word|
-    copy = input.clone
-    word = word.upcase.chomp
-    if input.length == word.length
-      word.each_char do |letter|
-        break unless copy.include?(letter)
-        copy = copy.sub(letter, "")
-      end
-      if copy == '' && word != input
-        anagrams << word
-      end
+    word = word.chomp
+    next unless word.length == input.length
+    if input.upcase.split("").sort == word.upcase.split("").sort
+      anagrams << word
     end
   end
   anagrams

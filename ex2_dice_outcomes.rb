97c9462975f8ae6fb41d1471bedc3e717a2e860a
Shannon Clark <theshannonclark@gmail.com>
2016-12-06 06:17:10 +0000

    Complete dice outcomes exercise

--- ex2_dice_outcomes.rb
+++ ex2_dice_outcomes.rb
@@ -2,7 +2,47 @@
 # roll and the number of times to roll them, then outputs a visual chart
 # of how many times each possible number comes up.
 
-def dice_outcomes(num_dice, times)
+def initialize_tally(num_dice)
+  tally = {}
+  min, max = num_dice, num_dice * 6
+  (min..max).to_a.each do |i|
+    tally[i.to_s] = 0
+  end
+  return tally
+end
+
+def roll_dice(num_dice = 1)
+  result = 0
+
+  die = (1..6).to_a
+  num_dice.times do
+    result += die.sample
+  end
+
+  return result
+end
+
+def roll_dice_n_times(num_dice, times_to_roll)
+  tally = initialize_tally(num_dice)
+
+  times_to_roll.times do
+    result = roll_dice(num_dice)
+    tally[result.to_s] += 1
+  end
+
+  return tally
+end
+
+def dice_outcomes(num_dice, times_to_roll)
+  result_tally = roll_dice_n_times(num_dice, times_to_roll)
+
+  outcomes = ""
+  result_tally.keys.each do |key|
+    outcomes += "\##{key}: #{"#" * result_tally[key]}\n"
+  end
+  puts outcomes
+
+  return result_tally
 end
 
 
@@ -24,4 +64,4 @@ dice_outcomes(3,100)
 #16: ##
 #17: ##
 #18: #
-#=> {3=>2, 4=>1, 5=>1, 6=>4, 7=>3, 8=>17, 9=>11, 10=>9, 11=>12, 12=>10, 13=>10, 14=>9, 15=>6, 16=>2, 17=>2, 18=>1} 
+#=> {3=>2, 4=>1, 5=>1, 6=>4, 7=>3, 8=>17, 9=>11, 10=>9, 11=>12, 12=>10, 13=>10, 14=>9, 15=>6, 16=>2, 17=>2, 18=>1}

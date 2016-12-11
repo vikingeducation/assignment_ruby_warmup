2e3ce825d0861eb295576abffca08ae9f0d5b1c0
Claire Till <tilly213@Yahoo.com>
2016-12-10 19:50:02 -0800

    Update roll_dice.rb

--- roll_dice.rb
+++ roll_dice.rb
@@ -1,11 +1,11 @@
-Write a method roll_dice which returns the result of rolling N 6-sided 
+#Write a method roll_dice which returns the result of rolling N 6-sided 
 dice where N is the input to the method (the default should be 1). For example:
 
-> roll_dice(4)
+#> roll_dice(4)
 #=> 18          # Rolled 4 dice
-> roll_dice(2)
+#> roll_dice(2)
 #=> 4           # Rolled 2 dice
-> roll_dice
+#> roll_dice
 #=> 1           # Rolled 1 die
 
 def roll_dice(n = 1)

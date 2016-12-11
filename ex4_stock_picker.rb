cd803377ce0730ca675ca440f9be88a28436f0e7
Shannon Clark <theshannonclark@gmail.com>
2016-12-11 01:44:02 +0000

    Complete stock picker exercise

--- ex4_stock_picker.rb
+++ ex4_stock_picker.rb
@@ -3,9 +3,25 @@
 # which to first buy the stock and then sell the stock,
 
 def stock_picker(prices)
+  min_index, max_index = -1, -1
+  min_diff = 1.0/0.0 # positive infinity
+
+  i, j = 0, 1
+  while i < prices.length - 1
+    while j < prices.length
+      if prices[i] - prices[j] < min_diff
+        min_diff = prices[i] - prices[j]
+        min_index, max_index = i, j
+      end
+      j += 1
+    end
+    i += 1
+    j = i + 1
+  end
+  return min_index, max_index
 end
 
 
 
-puts stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15]) 
+p stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
 #=> [2, 6]

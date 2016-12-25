c6f6c64a8a7d042fce315f45a02b443327c827d3
Stephen Akuffo <stephen.akuffo@gmail.com>
2016-12-25 16:05:00 -0500

    anagrams in method

--- anagrams.rb
+++ anagrams.rb
@@ -1,15 +1,15 @@
-dictionary = File.readlines('enable.txt').map(&:chomp)
+def anagram_finder(word_input, word_dictionary)
+  anagrams = []
+  possible_words = word_input.split('').permutation.map { |split_word| split_word.join('') }
+  possible_words.each do |word|
+    anagrams << word.upcase if word_dictionary.include?(word.downcase) && word != word_input
+  end
 
+  anagrams.uniq.sort
+end
+
+dictionary = File.readlines('enable.txt').map(&:chomp)
 print 'Please enter a word: '
 word_input = gets.chomp
 
-possible_words = word_input.split('').permutation.map { |split_word| split_word.join('') }
-
-anagrams = []
-possible_words.each do |word|
-  anagrams << word.upcase if dictionary.include?(word.downcase) && word != word_input
-end
-
-anagrams.uniq!
-
-print anagrams.sort
+print anagram_finder(word_input, dictionary)

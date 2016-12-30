cd0152eae7ed15f54fcf0a2fd7e14a55e2a18f9a
totoole38 <totoole38@gmail.com>
2016-12-30 10:48:17 -0700

    create hash of values and outcomes

--- methods.rb
+++ methods.rb
@@ -21,6 +21,64 @@
 	end
 
 
-puts roll_dice(2)
+# puts roll_dice(2)
 
 
+# ____________________________________________________
+# ******************Dice Outcomes**********************
+# ----------------------------------------------------
+
+def dice_outcomes(number_of_dice, times_to_roll)
+
+	# Create a hash to track the number of times each value is rolled. 
+	outcome_tracker = {}
+
+	# Roll all the dice times_to_roll times
+	times_to_roll.times do
+		
+		# Find a random number between 1 and 6 * number_of_dice 	
+		max_sum = 6*number_of_dice
+		outcome = rand(max_sum) + 1
+
+		# determine if the outcome is already a key in the outcome_tracker
+		if outcome_tracker.include?(outcome)
+			
+			#if it is, add 1 to its value
+			outcome_tracker[outcome] += 1
+
+			#if its not, create a key = to outcome and set its value to 1 
+			else
+				outcome_tracker[outcome] = 1
+		end
+	end
+
+
+
+	# Print the chart 
+
+
+
+
+
+
+return outcome_tracker
+
+
+end
+
+
+
+
+
+puts dice_outcomes(4, 20)
+
+
+
+
+
+
+
+
+# colorHash.each do |codeType, color|
+#   puts codeType + ':  ' + color
+# end

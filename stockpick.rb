404ad86b828eb9928357c99c5779760a6f49bf9c
Aaron Garza <robertaarongarza@gmail.com>
2017-01-06 13:14:22 -0500

    Finish stockpick app for pull request

--- stockpick.rb
+++ stockpick.rb
@@ -8,19 +8,10 @@ def stock_picker(stock_prices)
   max_profit = 0
   best_days = []
 
-  #Loop through stock prices taking in the buying price and the buying index.
-
   stock_prices.each_with_index do |buying_price, index|
-
-    #While in that loop, loop through stock prices adding one to get the sell price.
     (index+1).upto(stock_prices.length - 1) do |selling_index|
       selling_price = stock_prices[selling_index]
-
-    #check to see what the profit would be if you subtracted the buying price from the selling price.
       profit = selling_price - buying_price
-
-    #Update the maximum profit each time.  Use .max on the array to ensure you 
-    #return only the highest profit.
       if profit > max_profit
         max_profit = profit
         best_days = [index, selling_index]

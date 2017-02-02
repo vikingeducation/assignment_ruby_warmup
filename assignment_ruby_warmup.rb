eaf86d30ec0f575bf742ef770438b91ef23680c4
Robert Wayne <robertjwayne@gmail.com>
2017-02-02 11:29:49 -0500

    Add dice_outcomes method

--- assignment_ruby_warmup.rb
+++ assignment_ruby_warmup.rb
@@ -8,3 +8,26 @@ def roll_dice(dice_number = 1)
 
   return count
 end
+
+def dice_outcomes(dice_number, rolls)
+  roll_outcomes = {}
+
+  i = 1
+
+  while i <= (dice_number * 6)
+    roll_outcomes[i] = 0
+    i += 1
+  end
+
+  rolls.times do
+    roll = roll_dice(dice_number)
+    roll_outcomes[roll] += 1
+  end
+
+  roll_outcomes.each do |outcome, count|
+    print "#{outcome}: "
+    puts "#" * count
+  end
+
+  return roll_outcomes
+end

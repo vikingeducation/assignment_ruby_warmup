b126c868c08d6fa45ef94c3eddb1a4c0895993f4
Chad Lucas <chad.lucas.19@gmail.com>
2017-02-12 02:30:45 -0600

    add 1st half of dice_outcomes method

--- ruby_warmup_sprint.rb
+++ ruby_warmup_sprint.rb
@@ -1,3 +1,23 @@
 def roll_dice(n = 1)
-	puts n + rand(6)
+	#gets random number
+	puts n * rand(1..6)
 end
+
+#roll_dice(2)
+
+def dice_outcomes(dice, total_rolls)
+	#initiate empty array
+	dice_total_array = []
+	#rolls dice x amount of times
+	total_rolls.times do |i|
+      #gets random number and assigns to variable
+	  die_total = rand(1..6)
+      #puts roll of die into array
+	  dice_total_array << die_total
+	end
+
+	puts dice_total_array
+
+end
+
+dice_outcomes(2, 5)

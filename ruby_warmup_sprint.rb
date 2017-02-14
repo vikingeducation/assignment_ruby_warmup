2f386c694be03a35e6ad7963c1004e529633144d
Chad Lucas <chad.lucas.19@gmail.com>
2017-02-14 17:14:22 -0600

    add dice_outcome method

--- ruby_warmup_sprint.rb
+++ ruby_warmup_sprint.rb
@@ -1,23 +1,73 @@
+require 'pry'
 def roll_dice(n = 1)
 	#gets random number
 	puts n * rand(1..6)
 end
 
-#roll_dice(2)
-
 def dice_outcomes(dice, total_rolls)
+	dice_hash_table = Hash.new(0)
 	#initiate empty array
 	dice_total_array = []
 	#rolls dice x amount of times
 	total_rolls.times do |i|
       #gets random number and assigns to variable
-	  die_total = rand(1..6)
+	  die_total = dice * rand(1..6)
       #puts roll of die into array
 	  dice_total_array << die_total
+      end
+		puts dice_total_array
+	#puts dice_total array into hash and frequency of outcome
+	dice_total_array.each {|dice| dice_hash_table[dice] += 1}
+		puts dice_hash_table
+	dice_hash_table.each do |key, value|
+
+		puts "#{key}:#{'#' * value}"
 	end
 
-	puts dice_total_array
+end
+dice_outcomes(3, 10)
+
+def fibonacci(n)
+	results = []
+	n.times do |i|
+		
+		if i < 2
+			results << i
+		else
+			results << results[-1] + results[-2]
+		end
+	end 
+	puts results
+end
+
+def stock_picker(stocks)
 
 end
 
-dice_outcomes(2, 5)
+
+def anagrams(word)
+	#splits string into array and sorts it alpabetically
+	sorted_word = word.split("").sort
+	#creates empty array
+	anagrams_array = []
+	#gets enable file and puts it into an array
+	dictionary = File.open("enable.txt").readlines
+	#iterates through array
+	dictionary.each do |line|
+		#binding.pry
+		#splits and sorts word in array and matches w/ sorted_word
+		if line.split("").sort == sorted_word
+			#pushes word into array
+		 	anagrams_array.push(line)
+		 end 
+	end
+		
+
+end
+anagrams("pear")
+
+
+
+
+
+

684994a171a46772f3a2de8905ce4b57330b5b1b
Alex Thomas <home@Alexs-MacBook-Pro.local>
2017-02-24 02:44:15 +0900

    modified rolldice method and finished dice_outcomes

--- warmup.rb
+++ warmup.rb
@@ -1,40 +1,22 @@
 =begin
 	
 Write a method roll_dice which returns the result of 
-rolling N 6-sided dice where N is the input to the 
-method (the default should be 1). 
-For example:	
-> roll_dice(4)
-#=> 18          # Rolled 4 dice
-> roll_dice(2)
-#=> 4           # Rolled 2 dice
-> roll_dice
-#=> 1           # Rolled 1 die
+=end
+def rolldice(dies)
+	outcome = rand((1*dies)..(6*dies))	
+end
 
-Rolling Dice Outcomes
+p "Assignment 1"
+puts rolldice(4)
+puts "\n"
 
+=begin
 Now write a method dice_outcomes which takes the number 
 of dice to roll and the number of times to roll them, 
 then outputs a visual chart of how many times each 
 possible number comes up. 
 For example:
-=end
-def rolldice(dies)
 
-outcome = 0
-	3.times do 
-	  side = rand(1..6)	  
-		outcome = outcome + side
-	end
-	return outcome
-end
-
-p rolldice(3)
-
-
-
-
-=begin
 > dice_outcomes(3,100)
 3:  ##
 4:  #
@@ -54,3 +36,27 @@ p rolldice(3)
 18: #
 #=> {3=>2, 4=>1, 5=>1, 6=>4, 7=>3, 8=>17, 9=>11, 10=>9, 11=>12, 12=>10, 13=>10, 14=>9, 15=>6, 16=>2, 17=>2, 18=>1} 
 =end
+p "Assignment 2"
+def dice_outcomes(dies, roll)
+	outcome = 0
+	results = []
+	roll.times do
+	  outcome = rand((1*dies)..(6*dies))	
+	  results << outcome
+	  outcome = 0
+	end
+	list = results.sort
+	print list 
+	puts "\n"
+	list.each do |x|
+		recurrence = list.count(x)
+		puts "#{x} => #{recurrence}"
+		#puts "This is list before delete = #{list}"
+		list.delete(x)
+		list.unshift(x)
+		#puts "This is list after delete = #{list}"
+	end
+end
+
+dice_outcomes(4, 40)
+

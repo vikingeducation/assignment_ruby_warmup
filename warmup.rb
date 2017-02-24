33071fbf6e0ea045e9e5b0d6bee2a5c4bea4c5ff
Alex Thomas <home@Alexs-MacBook-Pro.local>
2017-02-25 01:28:30 +0900

    add fibonacci solution

--- warmup.rb
+++ warmup.rb
@@ -19,22 +19,8 @@ For example:
 
 > dice_outcomes(3,100)
 3:  ##
-4:  #
-5:  #
-6:  ####
-7:  ###
-8:  #################
-9:  ###########
-10: #########
-11: ############
-12: ##########
-13: ##########
-14: #########
-15: ######
-16: ##
-17: ##
-18: #
-#=> {3=>2, 4=>1, 5=>1, 6=>4, 7=>3, 8=>17, 9=>11, 10=>9, 11=>12, 12=>10, 13=>10, 14=>9, 15=>6, 16=>2, 17=>2, 18=>1} 
+
+#=> {3=>2, 4=>1, 5=>1, 6=>4, 7=>3, 18=>1} 
 =end
 p "Assignment 2"
 def dice_outcomes(dies, roll)
@@ -59,4 +45,85 @@ def dice_outcomes(dies, roll)
 end
 
 dice_outcomes(4, 40)
+puts "\n"
 
+=begin
+	
+Fibonacci
+
+Write a method fibonacci which outputs the Fibonacci Sequence 
+members out to the specified number of members. For example:
+
+> fibonacci(7)
+#=> [1, 1, 2, 3, 5, 8, 13]
+	
+=end
+p "Assignment 3"
+def fibonacci(nums)
+	first = 0
+	second = 1
+	outcome = 1
+	fibonacci_numbers = []
+	nums.times do
+		fibonacci_numbers << outcome
+		outcome = first + second
+		first = second
+		second = outcome
+	end
+	print fibonacci_numbers
+end
+
+puts fibonacci(7)
+
+=begin
+	
+Stock Picker
+
+Write a method stock_picker which takes an array of stock 
+prices (prices on days 0, 1, ...) and outputs the most profitable 
+pair of days on which to first buy the stock and then sell the stock, 
+for example:
+
+> stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15]) 
+#=> [2, 6]
+
+=end
+
+
+
+
+
+
+
+
+=begin
+Anagram Testing
+
+Write a method anagrams which returns an array of anagrams 
+that can be made with the specified string. Assume the 
+input is only a single word (e.g. "loot" not "William Shakespeare").
+
+Download enable.txt, a popular Scrabble dictionary, 
+and load it in as your dictionary of choice. If you haven't 
+encountered Ruby's file I/O yet, you can test your method 
+by simply providing an array of known anagrams like those 
+below to check that it works properly.
+
+You can use Dir.pwd to output the current directory. If you can't find 
+the dictionary, double check the directory you're executing your script 
+from within.
+
+Example:
+
+# Note: "pears" doesn't appear in the output
+> anagrams("pears")
+# => ["APERS","APRES","ASPER","PARES","PARSE","PRASE","PRESA","RAPES","REAPS","SPARE","SPEAR"]
+
+> anagrams("zygote")
+#=> []
+Finishing Up
+
+When you're finished with all tasks, push your changes up to your fork (aka $ git push origin master).
+To submit your assignment, create a pull request from your fork to the main upstream repository.
+	
+=end

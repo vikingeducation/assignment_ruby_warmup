0ee30b3c5ac8e8bda030e226c17d56a70dc6e965
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-03-13 12:53:46 -0500

    fibonacci.rb done

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,16 +5,17 @@
 #fib sequence each product is the product of the prior two products
 def fibonacci(n)
   fib_array = []
-  return 1 if n <= 2
-  fib_index = 3
+  fib_index = 1
   a, b = 1, 1
   while fib_index <= n
-    c = a + b
+    c = a
     a = b
-    b = c
+    b = c + b
+    fib_array << c
     fib_index += 1
   end
-  fib_array << c
+  return fib_array
 end
 
 print fibonacci(10)
+print fibonacci(14)

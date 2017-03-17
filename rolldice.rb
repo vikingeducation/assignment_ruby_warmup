9cf5a0850ff2737999305ff69e277d43c37d8e05
samuel-pedraza <samdpedraza@gmail.com>
2017-03-16 21:52:45 -0700

    finished chart thing

--- rolldice.rb
+++ rolldice.rb
@@ -1,8 +1,38 @@
-def rolldice(num)
-	puts rand(1..6) * num.to_i
+def roll_dice(times)
+	diceNumbers = []
+	while times > 0
+		diceNumbers.push(rand(1..6))
+		times -= 1
+	end
+
+	sum = 0
+	diceNumbers.each do |x|
+		sum += x	
+	end
+	puts sum
 end
 
-puts "input number of dice to throw"
-userinput = gets.chomp
+def roll_dice_2(times, amt)
+  
+  diceNumbers = []
+    (1..times).each do
+        diceNumbers.push(rand(1..6) * amt)
+	end
 
-rolldice(userinput)
+	obj = {}
+  
+  myRange = amt * 6
+
+	(amt..myRange).each do |x|
+		obj[x] = 0
+	end
+	
+    diceNumbers.each do |y|
+        num = diceNumbers.count(y)
+        obj[y] = num
+    end
+    
+    print obj
+end
+
+roll_dice_2(3, 3)

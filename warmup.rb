47aa8f2b70a338a96966a8f0d504a5b5a1f56ae1
Adam Jazairi <adamjazairi@gmail.com>
2017-03-27 00:12:05 -0400

    Add Fibonacci numbers method

--- warmup.rb
+++ warmup.rb
@@ -28,4 +28,19 @@ def dice_outcomes(number_of_dice, number_of_rolls)
 end
 
 
+def fibonacci(number)
+  fibonacci_numbers = [1, 1]
+
+  if number == 1
+    fibonacci_numbers = [1]
+  elsif number == 2
+    fibonacci_numbers = [1, 1]
+  else
+    (2..number-1).each { |i| fibonacci_numbers << fibonacci_numbers[i-2] + fibonacci_numbers[i-1] }
+  end
+
+  return fibonacci_numbers
+end
+
+
 binding.pry

7031f3778f62446517a4f71accd1deb11907d6ee
Tyler Ketron <tketron@gmail.com>
2017-03-29 21:21:23 -0700

    completed dice outcomes method

--- ruby_warmup.rb
+++ ruby_warmup.rb
@@ -5,3 +5,22 @@ def roll_dice(number_of_dice=1)
   end
   return sum
 end
+
+def dice_outcomes(number_of_dice, number_of_rolls)
+  #build hash of all possible values
+  outcomes = {}
+  (number_of_dice..number_of_dice*6).each do |i|
+    outcomes[i] = 0
+  end
+
+  #roll dice the specified number of times and store in hash
+  number_of_rolls.times do
+    roll = roll_dice(number_of_dice)
+    outcomes[roll] = outcomes[roll]+1
+  end
+
+  #output hash as formatted histogram
+  outcomes.each do |roll, tally|
+    puts "#{roll.to_s.ljust(2)}:  " + "#"*tally
+  end
+end

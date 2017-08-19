213e58829fdebeb106ee60ec0977d583d59e3885
jucavi <jcarvil@gmail.com>
2017-08-19 18:44:49 +0200

    Add dice_outcomes method

--- ruby_warmup.rb
+++ ruby_warmup.rb
@@ -3,3 +3,14 @@ def roll_dice(dice_number = 1)
   dice = dice_number - 1
   dice > 0 ? roll + roll_dice(dice) : roll
 end
+
+def dice_outcomes(dice_number, iteration)
+  hash_chart = Hash.new
+  iteration.times do
+    value = roll_dice(dice_number)
+    hash_chart.has_key?(value) ? hash_chart[value] += 1 : hash_chart[value] = 1
+  end
+  hash_chart = hash_chart.sort_by {|k, v| k }
+  hash_chart.each {|key, value| puts "#{key}:  "  +  "#"*value}
+  hash_chart.to_h
+end

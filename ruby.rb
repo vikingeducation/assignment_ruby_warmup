224781253eb90651b1303d9641bfcc19686e84bd
steven4354 <steven4354@gmail.com>
2017-08-28 15:48:04 -0700

    feat: created dice_outcomes method

--- ruby.rb
+++ ruby.rb
@@ -10,4 +10,24 @@ def roll_dice(dice_num) #returns the sum of the outcomes of rolling each dice on
 end
 
 
-  
+def dice_outcomes(dice_num, roll_times)
+
+  hash = {}
+
+  n = 1
+  while n <= roll_times do
+    sum = roll_dice(dice_num)
+    if hash[sum] == nil
+      hash[sum] = 1
+    else
+      hash[sum] += 1
+    end
+    n += 1
+  end
+
+  hash.each do |key,value|
+    p "#{key}: #{value}"
+  end
+end
+
+dice_outcomes(3, 100)

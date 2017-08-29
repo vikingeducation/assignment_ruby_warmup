c9ca37c588d853dd05dc8af30221fa1b6514a515
anne <richardson.ae@gmail.com>
2017-08-29 15:58:35 -0400

    Expand amagrams program to enable fetching text from a url

--- anagrams.rb
+++ anagrams.rb
@@ -1,4 +1,4 @@
-def anagrams(input)
+def local_anagrams(input)
   processed_input = input.downcase.chars.sort.join
   matches = []
 
@@ -16,4 +16,32 @@ def anagrams(input)
   end
 end
 
-anagrams('pears')
+local_anagrams('spear')
+
+#=======================================
+
+require 'open-uri'
+
+def cloud_anagrams(input)
+  puts "Fetching your anagram data from the web..."
+
+  web_dictionary = open('http://s3.amazonaws.com/viking_education/web_development/web_app_eng/enable.txt') {|f| f = f.readlines}
+
+  processed_input = input.downcase.chars.sort.join
+  matches = []
+
+  web_dictionary.each do |word|
+    word = word.sub("\n", "")
+    processed_word = word.downcase.chars.sort.join
+    matches << word if (processed_word == processed_input) && (word != input)
+  end
+
+  if matches.length > 0
+    puts "Anagrams for '#{input}' are:"
+    puts matches
+  else
+    puts "Sorry, there are no anagrams for '#{input}'."
+  end
+end
+
+cloud_anagrams('pears')

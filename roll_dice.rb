219a1bffdc858d9b226d3cae278b000f47c06571
mac718 <mac718@gmail.com>
2017-09-02 15:13:26 -0400

    wrote dice_outcomes method

--- roll_dice.rb
+++ roll_dice.rb
@@ -1,9 +1,23 @@
 def roll_dice(number_of_dice = 1)
   die = [1, 2, 3, 4, 5, 6]
   die_rolls = []
-  roll_total = 0 
   
   number_of_dice.times { die_rolls << die.sample }
-  
+
   roll_total = die_rolls.reduce(:+)
+end 
+
+def dice_outcomes(number_of_dice = 1, number_of_rolls = 1)
+  die = [1, 2, 3, 4, 5, 6]
+  roll_totals = []
+  
+  number_of_rolls.times do 
+    die_rolls = []
+    number_of_dice.times { die_rolls << die.sample }
+    roll_totals << die_rolls.reduce(:+)
+  end
+  
+  (number_of_dice..(number_of_dice * 6)).each do |num|
+    puts "#{num}" + ': ' + "#" * roll_totals.count(num)
+  end
 end 

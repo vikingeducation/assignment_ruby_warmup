915135e252d3edbc3e0e1fa4564d511ce018ae5e
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-08 23:20:41 -0500

    completed third part, fibonacci sequence generator

--- Ruby_Warmup.rb
+++ Ruby_Warmup.rb
@@ -10,5 +10,27 @@ def roll_dice(n = 1)
     end
 end
 
-roll_dice(4)
-roll_dice(2)
+#roll_dice(4)
+#roll_dice(2)
+
+def dice_outcomes(dice, rolls)
+  # come back to this one
+end
+
+def fibonacci(limit)
+  first = 1
+  second = 1
+  sequence = []
+  counter = 0
+  while counter < limit
+    sequence.push(first)
+    increment = first + second
+    first = second
+    second = increment
+    counter += 1
+  end
+  puts "The numbers in the Fibonacci Sequence up to #{limit.to_s} are #{sequence}"
+end
+
+#fibonacci(4)
+#fibonacci(7)

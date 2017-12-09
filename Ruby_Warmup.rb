46a1d4d81e0fcef0be844153d1e18043d9615b0f
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-09 01:13:59 -0500

    completed Stock Picker

--- Ruby_Warmup.rb
+++ Ruby_Warmup.rb
@@ -34,3 +34,34 @@ end
 
 #fibonacci(4)
 #fibonacci(7)
+
+def stock_picker(array)
+  count = 0
+  largest = 0
+  step = 1
+  result = 0
+  while count < array.length - 1
+    while step < array.length
+      result = array[step] - array[count]
+      if result > 0
+        if result > largest
+          largest = result
+          low = count
+          high = step
+        end
+      end
+      step += 1
+    end
+    count += 1
+    step = count + 1
+  end
+    if low == nil
+      puts "There was never a good day to buy and sell the stock"
+    else
+      puts "The best day to buy the stock was day #{low.to_s} at #{array[low].to_s}, and the best day to sell was day #{high.to_s} at #{array[high].to_s}"
+    end
+end
+
+#stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+#stock_picker([500, 700, 300, 400, 200, 100, 600])
+#stock_picker([5, 4, 3, 2, 1])

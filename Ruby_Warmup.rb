d59d753a6a5dded3a3e6e9ddc1eb897cf5eff884
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-10 22:29:13 -0500

    saving progress on dice_outcomes

--- Ruby_Warmup.rb
+++ Ruby_Warmup.rb
@@ -14,12 +14,65 @@ end
 #roll_dice(2)
 
 def dice_outcomes(dice, rolls)
-  
+  tracker = 0
+  outcomes = []
+  while rolls > tracker
+    growth = 0
+    outcome = 0
+    while dice > growth
+      outcome += 1 + rand(6)
+      growth += 1
+    end
+    outcomes.push(outcome)
+    tracker += 1
+  end
+puts " The result was #{outcomes.to_s} "
+    chart = []
+    gauge = 0
+    while outcomes.length > gauge
+      index = outcomes[gauge]
+      if !chart.include?(index)
+        chart.push(index)
+      end
 
+      if chart.include?(index)
+        if index != index.to_s
+          chart.select! { |index| index = "#{index.to_s}: " }
+        end
+        chart.select! { |index| index = "#{index}#" }
+      end
 
-
-  # use a hash/array for the visual chart
+      gauge += 1
+    end
+puts "the chart looks like this #{chart}"
 end
+
+=begin
+#how output array should look
+puts array = [ "1: #",
+   "2: #####"]
+=end
+
+array = []
+limit = 0
+base = [4, 5, 6, 1, 4, 2, 3, 4]
+arr = []
+
+# loop through the values in base and if their unique push them into arr
+
+arr.sort!
+while limit < arr.length
+arr.map! { |x|
+  if(x != x.to_s)
+    x = x.to_s
+  end
+  if x != "#{x}: " && "" == x.gsub(/\d/, '')
+    array.push(x = x + ": ")
+  end
+  limit += 1
+}
+end
+puts "#{arr}, #{array}, #{base}"
 
 #dice_outcomes(3, 100)
 #dice_outcomes(2, 1)
